cd281985eba8a37dcca19c8bc3c86e1ba24df489
borossandor27 <boros.sanyi@gmail.com>
2025-07-04 09:24:48 +0200

    1.2.1

---
+++ csharpElmeletSolution/PilotakFeladat/Pilota.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilotakFeladat
+{
+    internal class Pilota
+    {
+        public string nev;
+        public DateTime szuletesi_datum;
+        public string nemzetiseg;
+        public string rajtszam;
+
+        public Pilota(string beolvasottSor)
+        {
+            string[] adatok = beolvasottSor.Split(';');
+            if (adatok.Length < 3)
+            {
+                throw new ArgumentException("A sor nem tartalmazza a szükséges adatokat.");
+            }
+            this.nev = adatok[0];
+            this.szuletesi_datum = DateTime.Parse(adatok[1]);
+            this.nemzetiseg = adatok[2];
+            // Csak az aktív pilóták esetében van rajtszám!
+            this.rajtszam = adatok[3];
+        }
+    }
+}
--- csharpElmeletSolution/PilotakFeladat/Program.cs
+++ csharpElmeletSolution/PilotakFeladat/Program.cs
@@ -8,8 +8,23 @@ namespace PilotakFeladat
 {
     internal class Program
     {
+        public static List<Pilota> pilotak = new List<Pilota>();
         static void Main(string[] args)
         {
+            Console.WriteLine("Program indul...");
+            pilotaAdatokBeolvasasa();
+            Console.WriteLine("\nProgram vége!");
+            Console.WriteLine("Nyomj meg egy gombot a kilépéshez...");
+            Console.ReadKey();
+        }
+
+        private static void pilotaAdatokBeolvasasa()
+        {
+            string[] sorok = System.IO.File.ReadAllLines("pilotak.csv", Encoding.UTF8).Skip(1).ToArray();
+            foreach (string sor in sorok)
+            {
+                pilotak.Add(new Pilota(sor));
+            }
         }
     }
 }
